025dfda4898272c724afabbde1d9033f443cf36a
Jon Yu <jy300x2@gmail.com>
2016-07-27 23:15:03 +0000

    Added weight and eye color to Cats

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712230746) do
+ActiveRecord::Schema.define(version: 20160727231315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20160712230746) do
     t.datetime "updated_at"
     t.string   "color"
     t.integer  "age"
+    t.integer  "weight"
+    t.string   "eye_color"
   end
 
 end
